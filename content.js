7fbff10e904e783ce833e097596683b29cead3b7
blockheads <nicholaspueschel@gmail.com>
2021-04-12 14:23:54 -0400

    updated to play quiter, and play first 12 sounds

--- content.js
+++ content.js
@@ -1,13 +1,21 @@
 waitForChat();
 
+// this is how many taunts we have available, we assume that we have 
+// 1- this specified number of taunts all initialized in the 'sounds' folder.
+TAUNT_SIZE = 12;
+
 // this is what we use to observer the DOM to get twitch chat updates.
 TWITCH_CHAT_CLASS = ".chat-scrollable-area__message-container";
+TWITCH_CHAT_MESSAGE_CLASS = ".text-fragment";
+
+// path to our sounds folder
+SOUNDS_PATH = "sounds/"
+// OGG suffix
+SOUND_FILE_SUFFIX = ".ogg"
 
 function initiateObservers() {
 
-    console.log("initiating observer for chat!");
-
-    var target = document.querySelector(".chat-scrollable-area__message-container");
+    var target = document.querySelector(TWITCH_CHAT_CLASS);
 
     var observer = new MutationObserver(function(mutations) {  
         mutations.forEach(function(mutation) {
@@ -19,9 +27,19 @@ function initiateObservers() {
                 
                 // extract the chat message
                 var message = extractChatMessage(chatNode);
+                var taunt = "";
 
-                // now we can pass our message into whatever to play audio
-                parseMessage(message);
+                // now we can pass our message to get the taunt if there is one
+                if(!(message === "")){
+                    taunt = parseMessage(message);
+                }
+
+                // play the audio corresponding to the taunt
+                if(!(taunt === "")){
+                    playTaunt(taunt);
+                }
+                else
+                    console.log("message did not contain a taunt.");
             }
                 
                 
@@ -35,25 +53,24 @@ function initiateObservers() {
 }
 
 function extractChatMessage(chatNode){
-    return chatNode.querySelector(".text-fragment").textContent;
+
+    let messageNode = chatNode.querySelector(TWITCH_CHAT_MESSAGE_CLASS);
+    if(messageNode == null)
+        return "";
+
+    return messageNode.textContent;
 }
 
 function documentOnLoad(){
     initiateObservers();
 }
 
-function playSound(){
-
-    var audio = new Audio(chrome.extension.getURL("sounds/Yes.ogg"));
-    audio.play();
-
-}
 function waitForChat() {
     const time0 = Date.now();
     const int = setInterval(() => {
         if (Date.now() - time0 > 10 * 1000) clearInterval(int);
-        chat = document.querySelector(".chat-scrollable-area__message-container");
-        console.log("found chat " +  chat);
+        chat = document.querySelector(TWITCH_CHAT_CLASS);
+
         if (chat) {
             clearInterval(int);
             documentOnLoad();
@@ -64,17 +81,43 @@ function waitForChat() {
 // this function attempts to parse a message ad executure the corresponding AOE sound
 function parseMessage(message){
 
-    var audio = null;
+    // go through and find the first taunt matching our string
 
-    if(message.startsWith("1") || message.endsWith("1")){
-        var audio = new Audio(chrome.extension.getURL("sounds/Yes.ogg"));
+    // I decided to use a parseInt to make it behave true to the way
+    // aoe taunts appear to work, we can do other behaviors later
+    var potentialTaunt = parseInt(message);
+    // first we check the front
+    for(var i=TAUNT_SIZE; i >= 1; i--){
+
+        if(i == potentialTaunt)
+            return String(i);
+        
     }
 
-    if(!audio){
-        console.log("Message didn't contain an emote");
-        return;
-    }
+    // then the back, I decided to detect taunts that were at the end
+    // of messages since some chatters preffer that
+    // deleting this for now to make it act like the game, maybe add back later
+    // for(var i=TAUNT_SIZE; i >= 1; i--){
+    //     var tauntString = String(i);
 
-    audio.play();
+    //     if(message.endsWith(tauntString))
+    //         return tauntString;
+        
+    // }
+    
+    // just return the empty string otherwise...
+    return "";
+
+}
+
+function playTaunt(tauntString){
+
+    // alternatively we can load each taunt on startup...
+    let filePath = SOUNDS_PATH + tauntString + SOUND_FILE_SUFFIX;
+    console.log("playing taunt: " + filePath);
+    
+    var tauntAudio = new Audio(chrome.extension.getURL(filePath));
+    tauntAudio.volume = .2;
+    tauntAudio.play();
 
 }
