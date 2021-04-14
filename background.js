0ffcdb0965c6e1c075dae5fc62c820c5e789cfd2
blockheads <nicholaspueschel@gmail.com>
2021-04-14 00:29:48 -0400

    new icons, and added popup menu

--- background.js
+++ background.js
@@ -11,6 +11,24 @@ chrome.tabs.onUpdated.addListener(function
     }
   }
 );
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
+  });
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        // we only really want our popup menu on twitch.tv :)
+        pageUrl: {hostEquals: "www.twitch.tv"},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
 // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 //     if (changeInfo.status == 'complete') {
 //       if (tab.url.indexOf("twitch.tv") != -1) {
